70108c7c92f2b4a5f272b8fed226cbfa77268270
afeiship <1290657123@qq.com>
2023-02-05 14:34:42 +0800

    feat: add baseURL option

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,20 @@ import NxLcOptions from '@afeiship/next-leancloud-options';
 
 const WEIBO_LC_ID = '60f768f6d9f1465d3b1d5c43';
 const fetch = window.fetch.bind(window);
+const defaults = {
+  baseURL: 'https://tva1.js.work'
+};
 
 export default class {
   private loader;
   private lcOpts;
+  private options;
   private weiboOss;
 
-  constructor(inLoader) {
+  constructor(inLoader, inOptions) {
     this.loader = inLoader;
     this.lcOpts = new NxLcOptions({ fetch, id: WEIBO_LC_ID });
+    this.options = { ...defaults, ...inOptions };
   }
 
   upload() {
@@ -31,9 +36,10 @@ export default class {
 
   private weiboOssClient() {
     if (this.weiboOss) return Promise.resolve(this.weiboOss);
+    const { baseURL } = this.options;
     return new Promise((resolve) => {
       this.lcOpts.get().then((res) => {
-        this.weiboOss = new NxWeiboOss(res.value);
+        this.weiboOss = new NxWeiboOss(res.value, { baseURL });
         resolve(this.weiboOss);
       });
     });
